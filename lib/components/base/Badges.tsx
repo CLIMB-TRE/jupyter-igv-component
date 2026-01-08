19e0f244b34d50ccf7e2ce22f1b548fc8c490f9f
tombch <t.brier@outlook.com>
2026-01-08 15:32:03 +0000

    Cleaner required/optional fields in light and dark mode

--- lib/components/base/Badges.tsx
+++
@@ -1,9 +0,0 @@
-import Badge, { BadgeProps } from "react-bootstrap/Badge";
-
-export function OptionalBadge(props: BadgeProps) {
-  return (
-    <Badge bg="light" className="mx-2" style={{ color: "gray" }} {...props}>
-      Optional
-    </Badge>
-  );
-}
